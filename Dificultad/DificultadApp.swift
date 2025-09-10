fcbdc56c60b8c5c3082e44eb9e0af60a45c9f642
Marcelo <marcelo.neri@iest.edu.mx>
2025-09-10 17:40:12 -0600

    Initial Commit

---
+++ Dificultad/DificultadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DificultadApp.swift
+//  Dificultad
+//
+//  Created by win603 on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DificultadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
